39ef1313494d7fb5d6b474beb428202f2391b18e
Arsenii Yamnii <yamnii@ya.ru>
2021-03-27 20:49:43 +0500

    clean whitestrings

--- render.py
+++ render.py
@@ -2,7 +2,6 @@
 import pystache
 import json
 import os
-
 def chooser(ask, value):
     print(ask)
     n=0
@@ -11,33 +10,20 @@ def chooser(ask, value):
         n+=1
     number = int(input())
     return(value[number])
-
 with open('yabtm.conf.json') as json_file:
     config = json.load(json_file)
-#print(config)
 query = {}
 query["projectPath"] = str(os.getcwd())[:-11]
 for i in config["fields"]:
-    #    if(isinstance(i, str)):
-    #print(config["fields"][i])
- #   print(type(config["fields"][i]['value']))
     if(isinstance(config["fields"][i]['value'], list)):
-       #config["fields"][i]["temp"] = chooser(config["fields"][i]["ask"],config["fields"][i]["value"])
        answ = chooser(config["fields"][i]["ask"],config["fields"][i]["value"])
        query[answ]=True
     else:
-        #config["fields"][i]["temp"]= input(config["fields"][i]["ask"])
         query[i]= input(config["fields"][i]["ask"])
-
 for i in config["files"]:
     temp_file = open("template/"+i, "r")
     outfile = pystache.render(temp_file.read(), query)
-    #print(outfile)
     temp_file.close()
     temp_file = open("template/"+i, "w")
     temp_file.write(outfile)
 
-#print(config["fields"])
-
-
-#print(pystache.render('Hi {{person}}!', {'person': 'Mom'}))
